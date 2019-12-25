71e6e1e1358aca97b4b878417539426f0fac704b
GraceeFlower <1491644103@qq.com>
2019-12-25 20:07:50 +0800

    [WangMin] feat: choose a mole out of its hole & down in a random interval after clicking the start button

--- whack-a-mole/game-normal.js
+++ whack-a-mole/game-normal.js
@@ -1,36 +1,54 @@
 window.onload = function () {
 
-    const holes = document.querySelectorAll('.hole');
-    const scoreBoard = document.querySelector('.score');
-    const moles = document.querySelectorAll('.mole');
-    const startBtn = document.getElementById('start_btn');
-    let titleH1 = document.getElementById('title');
+  const holes = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('.score');
+  const moles = document.querySelectorAll('.mole');
+  const startBtn = document.getElementById('start_btn');
+  let titleH1 = document.getElementById('title');
 
-    let lastHole;
-    let timeUp = false;
-    let score = 0;
-    let gameTime = 10000;
-
-
-    startBtn.addEventListener('click', function () {
-        showBtnAnimation();
-        startGame();
-    }, false);
-
-    function showBtnAnimation() {
-        event.preventDefault();
-
-        startBtn.classList.add('animate');
-        // 按钮动画延时，按钮动画结束后发生的事：换为正常状态（class中的animate去掉），开始按钮消失
-        setTimeout(() => {
-            startBtn.classList.remove('animate');
-            startBtn.style.display = 'none';
-        }, 700);
-    }
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+  let gameTime = 10000;
 
 
-    function startGame() {
-        // TODO: 写开始新游戏后发生的事
-    }
+  startBtn.addEventListener('click', function () {
+    showBtnAnimation();
+    setTimeout(() => {startGame()}, 1000);
+  }, false);
 
+  function showBtnAnimation() {
+    event.preventDefault();
+
+    startBtn.classList.add('animate');
+    // 按钮动画延时，按钮动画结束后发生的事：换为正常状态（class中的animate去掉），开始按钮消失
+    setTimeout(() => {
+      startBtn.classList.remove('animate');
+      startBtn.style.display = 'none';
+    }, 700);
+  }
+
+  function startGame() {
+  // TODO: 写开始新游戏后发生的事
+    const time = randomTime(500, 1000);
+    const hole = randomHole();
+    peep(time, hole);
+  }
+
+  function peep(holeOutTime, holeIndex) {
+    var hole = document.querySelector(`.${holeIndex}`);
+    hole.classList.add("up");
+    setTimeout(() => {
+        hole.classList.remove("up");
+    }, holeOutTime);
+  }
+
+  function randomTime(minInterval, interval) {
+    return minInterval + Math.random() * interval;
+  }
+
+  function randomHole() {
+    var holeIndex = "hole" + Math.ceil(Math.random() * 6);
+    return holeIndex;
+  }
 };
